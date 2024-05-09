beaf197670c07555e365beaffed8a7b4ac04b17d
Ashlyn Wise <ashlynw@vt.edu>
2024-05-09 04:40:22 -0400

    organize into modules

--- MonteCarlo/MonteCarlo/__init__.py
+++ MonteCarlo/MonteCarlo/__init__.py
@@ -2,6 +2,6 @@
 
 # Add imports here
 from .functions import *
-
-
+from .isinghamiltonian import get_IsingHamiltonian
+from .bitstring import Bitstring
 from ._version import __version__
--- MonteCarlo/MonteCarlo/functions.py
+++ MonteCarlo/MonteCarlo/functions.py
@@ -1,19 +1,12 @@
 """Provide the primary functions."""
 
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import random
+import math
 
 def get_IsingHamiltonian(G, mus=None):
-    if mus == None:
-        mus = np.zeros(len(G.nodes()))
-
-    if len(G.nodes()) != len(mus):
-        error("DimensionMismatch")
-
-    if len(G.nodes()) != len(mus):
-        error(" Dimension Mismatch")
-    J = [[] for i in G.nodes()]
-    for e in G.edges:
-        J[e[0]].append((e[1], G.edges[e]['weight']))
-        J[e[1]].append((e[0], G.edges[e]['weight']))
     return montecarlo.IsingHamiltonian(J,mus)
 
 if __name__ == "__main__":
